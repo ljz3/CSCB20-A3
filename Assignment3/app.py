48c1fe3eb57e85570c158378a7ef1813120b3431
Jawad Arshad <56083056+HamJaw1432@users.noreply.github.com>
2021-04-02 13:45:56 -0400

    added backend for remark page

---
+++ Assignment3/app.py
@@ -0,0 +1,47 @@
+import sqlite3
+from flask import Flask, render_template, request, g, escape, url_for, redirect, session
+
+DATABASE = './assignment3.db'
+app = Flask(__name__)
+
+access_username = ''
+isadmin = True
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()    
+
+@app.route('/')
+def remark():
+    db = get_db()
+    db.row_factory = make_dicts
+
+    remarks_dict = []
+    if isadmin == True:
+        for want_remark in query_db('select * from Remarks'):
+            remarks_dict.append(want_remark)
+        db.close()
+        return render_template('remark.html', remarks=remarks_dict)
+    
+

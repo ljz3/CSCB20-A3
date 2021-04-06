5d5f9ed910fa2b9a915d5570138c208012644eaa
Jawad Arshad <56083056+HamJaw1432@users.noreply.github.com>
2021-04-05 23:09:47 -0400

    added renders to other static pages

--- Assignment3/app.py
+++ Assignment3/app.py
@@ -43,6 +43,12 @@ def remark():
             remarks_dict.append(want_remark)
         db.close()
         return render_template('remark.html', remarks=remarks_dict)
+    else:
+        for want_remark in query_db('select * from Remarks where userneme =' + access_username):
+            remarks_dict.append(want_remark)
+        db.close()
+        return render_template('remark.html', remarks=remarks_dict)
+
 
 @app.route('/grade')
 def get_assid_grade():
@@ -74,9 +80,44 @@ def login():
     if request.method=='POST':
         sql = """
 	    SELECT *
-	    FROM Login
+	    FROM Login NATURAL JOIN Person
 	    """
         results = query_db(sql, args=(), one=False)
         for result in results:
             if result[1]==request.form['Username']:
                 if result[2]==request.form['Password']:
+                       return render_template('grade.html', grade=grades)
+
+
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
+
+@app.route('/assignments.html')
+def assignments():
+    return render_template('assignments.html')
+
+@app.route('/courseteam.html')
+def courseteam():
+    return render_template('courseteam.html')
+
+@app.route('/feedback.html')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/labs.html')
+def labs():
+    return render_template('labs.html')
+
+@app.route('/markus.html')
+def markus():
+    return render_template('markus.html')
+
+@app.route('/piazza.html')
+def piazza():
+    return render_template('piazza.html')
+
+@app.route('/syllabus.html')
+def syllabus():
+    return render_template('syllabus.html')
+
